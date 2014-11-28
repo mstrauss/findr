b17ca688134bcc7558683220495adbcf9fbdeeda
Markus Strauss <git@ms.sieb.mx>
2014-11-28 13:46:44 +0100

    ~ StrategyProxy now correctly validates methods

--- lib/findr/strategy_proxy.rb
+++ lib/findr/strategy_proxy.rb
@@ -20,25 +20,23 @@ module Findr
           caller.first.match(/in \`(.+)\'/)
           method_name = $1
         end
-        raise MethodNotImplementedError.new("#{klass.class.name} needs to implement '#{method_name}' for StrategyProxy #{self.name}!")
+        raise MethodNotImplementedError.new("#{klass.class.name} needs to implement '#{method_name}' for StrategyProxy #{self.class.name}!")
       end
 
     end
 
     module ClassMethods
 
-      def provides(method_name, *args)
-        puts "providing method #{method_name} with args #{args.inspect}"
-        this = self
-        self.class_eval do
-          define_method(method_name) do |*arg|
-            begin
-              @strategy.send(method_name, *arg)
-            rescue NoMethodError
-              this.does_not_implement_method(@strategy, method_name)
-            end
+      def provides(method_name, *argument_names)
+        arglist = argument_names.map(&:to_sym).join(',')
+        method_string = <<-END_METHOD
+          def #{method_name}(#{arglist})
+            @strategy.#{method_name}(#{arglist})
+          rescue NoMethodError
+            does_not_implement_method(@strategy, '#{method_name}')
           end
-        end
+        END_METHOD
+        class_eval method_string
       end
 
     end
